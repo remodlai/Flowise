676101589c99712ad2eb17e0d934ae4b45d1b9cd
Brian Bagdasarian <brian@brianbagdasarian.com>
2025-03-13 22:54:54 -0400

    m

--- packages/server/src/utils/case.ts
+++ packages/server/src/utils/case.ts
@@ -12,11 +12,20 @@ import { isCamelCase, isSnakeCase, constantCase, dotCase, sentenceCase } from 'u
 
 export class CaseMaker {
 
-
+    /**
+     * Check if a string is in camelCase.
+     * @param str - The string to check.
+     * @returns True if the string is in camelCase, false otherwise.
+     */
     isCamelCase(str: string) {
         return isCamelCase(str)
     }
 
+    /**
+     * Check if a string is in snake_case.
+     * @param str - The string to check.
+     * @returns True if the string is in snake_case, false otherwise.
+     */
     isSnakeCase(str: string) {
         return isSnakeCase(str)
     }
@@ -46,15 +55,35 @@ export class CaseMaker {
         return constantCase.output
     }
 
+    /**
+     * Convert a string from snake_case to dotCase.
+     * @param str - The string to convert.
+     * @returns The converted string in dotCase.
+     */
     toDotCase(str: string) {
         const dotCase = getCaseConverter('dotCase')(str)
         return dotCase.output
     }
 
+    /**
+     * Convert a string from snake_case to sentenceCase.
+     * @param str - The string to convert.
+     * @returns The converted string in sentenceCase.
+     */
     toSentenceCase(str: string) {
         const sentenceCase = getCaseConverter('sentenceCase')(str)
         return sentenceCase.output
     }   
+
+    /**
+     * Convert a string from snake_case to pathCase.
+     * @param str - The string to convert.
+     * @returns The converted string in pathCase.
+     */
+    toPathCase(str: string) {
+        const pathCase = getCaseConverter('pathCase')(str)
+        return pathCase.output
+    }
 
 }
 
